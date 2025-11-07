35b6cf19047fbee4db4b0f72091d01f1df96aa47
Nils Alexander Haberkamp <N.Haberkamp@sw-gg924j9rwy.home>
2025-11-07 10:20:47 +0100

    docs(nextjs): add Accordion component example

--- playground/nextjs/src/app/page.tsx
+++ playground/nextjs/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { EditableArea, EditableCancelTrigger, EditableEditTrigger, EditableInput, EditablePreview, EditableRoot, EditableSubmitTrigger } from 'reka-ui-react'
+import { AccordionContent, AccordionHeader, AccordionItem, AccordionRoot, AccordionTrigger } from '../../../../packages/react/src/Accordion'
 
 export default function Home() {
   return (
@@ -31,7 +32,42 @@ export default function Home() {
           </EditableRoot>
         </div>
 
-        <div className="text-white">hi</div>
+        <div className="w-full h-full grid place-items-center rounded-xl border border-stone-700 p-6 min-h-[300px]">
+          <AccordionRoot className="w-full max-w-md">
+            <AccordionItem value="item-1">
+              <AccordionHeader>
+                <AccordionTrigger className="text-white">
+                  Accordion Item 1
+                </AccordionTrigger>
+              </AccordionHeader>
+              <AccordionContent className="text-gray-300 p-4">
+                This is the content for accordion item 1.
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-2">
+              <AccordionHeader>
+                <AccordionTrigger className="text-white">
+                  Accordion Item 2
+                </AccordionTrigger>
+              </AccordionHeader>
+              <AccordionContent className="text-gray-300 p-4">
+                This is the content for accordion item 2.
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-3">
+              <AccordionHeader>
+                <AccordionTrigger className="text-white">
+                  Accordion Item 3
+                </AccordionTrigger>
+              </AccordionHeader>
+              <AccordionContent className="text-gray-300 p-4">
+                This is the content for accordion item 3.
+              </AccordionContent>
+            </AccordionItem>
+          </AccordionRoot>
+        </div>
       </div>
     </div>
   )
